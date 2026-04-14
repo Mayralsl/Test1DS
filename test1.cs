97df0facb394b5b756b7d74f8a4d0cea013201ba
Mayralsl <mayraluiza2910@gmail.com>
2026-04-14 15:21:43 -0300

    Test1-semlayout

---
+++ test1.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaAcademicoEPSA.Models
+{
+    public class Materias
+    {
+        public string nomeDaMateria { get; set; }
+        public string professorResponsavel { get; set; }
+        public int numeroDaSala { get; set; }
+    }
+}
